65d060408125a52802cbf8196b398516108af4e6
Vighanesh Gaund <103332350+vighanesh2@users.noreply.github.com>
2023-08-16 02:20:37 +0400

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,26 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 4000); // Change slide every 4 seconds
 }
+
+  // Smooth scroll function
+  function smoothScroll(target) {
+    const targetElement = document.querySelector(target);
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth'
+      });
+    }
+  }
+
+  // Add click event listeners to navigation links
+  document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(link => {
+      link.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent default link behavior
+        const target = link.getAttribute('href');
+        smoothScroll(target);
+      });
+    });
+  });
